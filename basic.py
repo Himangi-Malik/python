a6dc26f792fc52d2921c719e9025fce7b91c06c6
Himangi Malik <122164812+Himangi-Malik@users.noreply.github.com>
2023-10-02 19:45:05 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,23 @@
+from math import *
+
+char_name = "john"
+char_age = "35"
+is_male = True
+print(" the name of the person is " + char_name + " pakak")
+print(" the age of the person is " + char_age + " pakak")
+
+str1 = "Giraffe Academy"
+print(str1.lower())
+print(str1.upper())
+print(str1.isupper())
+print(str1.upper().isupper())
+print(len(str1))
+print(str1.index("a"))
+print(str1.replace("Giraffe","Pakak" ))
+
+my_num = 5
+print(str(my_num) + " is awesome")
+num_2 = -12
+print(abs(num_2))
+print(floor(4.873))
+print(ceil(4.873))
