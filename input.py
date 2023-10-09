a48b6999af9d015ac6213a8cd6cfc9baffd45c93
Himangi Malik <122164812+Himangi-Malik@users.noreply.github.com>
2023-10-09 19:26:14 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,9 @@
+#i am supreme pakak
+name = input("Enter your name : ")
+print ("hello " + name )
+
+num1 = input(" enter a number: ")
+num2 = input(" enter a number: ")
+
+result = int(num1) + int(num2)
+print ( result)
